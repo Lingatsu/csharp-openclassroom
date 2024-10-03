0f3a25121b8414f2385594f39f2dc2f9c48e92c4
Lingatsu <115959500+Lingatsu@users.noreply.github.com>
2024-10-03 14:45:45 +0200

    Exercices P1 terminé

--- P1/P1C3.1/Program.cs
+++ P1/P1C3.1/Program.cs
@@ -2,7 +2,5 @@
 int denominateur = 4;
 
 // TODO : modifier l'instruction ci-dessous pour que la réponse prenne une valeur décimale
-int reponse = numerateur / denominateur;
-
-// Afficher le résultat 
-Console.WriteLine(numerateur + " / " + denominateur + " = " + reponse);
+double reponse = numerateur / (double) denominateur;
+Console.WriteLine(reponse);
--- P1/P1C5.4/Program.cs
+++ P1/P1C5.4/Program.cs
@@ -1,19 +1,23 @@
 ﻿IDictionary<string, int> mois = new Dictionary<string, int>();
 
 // TODO : Remplacer les noms par des constantes de type chaine de caractères
-mois.Add("Juin", 6);
-mois.Add("Septembre", 9);
-mois.Add("Mars", 5);
+const string Juin = "Juin";
+const string Septembre = "Septembre";
+const string Mars = "Mars";
+
+mois.Add(Juin, 6);
+mois.Add(Septembre, 9);
+mois.Add(Mars, 5);
 
 // TODO : Corriger la valeur de Mars avec (3)
-
+mois[Mars]=3;
 
 // TODO : Retirer Juin
-
+mois.Remove(Juin);
 
 // Afficher le contenu du dictionnaire
 Console.WriteLine("La liste des mois est :");
 foreach (KeyValuePair<string, int> clePaire in mois)
 {
-    Console.WriteLine(clePaire.Key.ToString() + " est le numéro du mois de " + clePaire.Value + " de l'année ");
+    Console.WriteLine(clePaire.Value + " est le numéro du mois de " + clePaire.Key.ToString() + " de l'année ");
 }
