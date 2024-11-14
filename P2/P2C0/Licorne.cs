1b48cb5c948f819ed353c44e70af8bf932fed89a
Lingatsu <115959500+Lingatsu@users.noreply.github.com>
2024-11-14 17:02:13 +0100

    Exercice P2C0 terminé

---
+++ P2/P2C0/Licorne.cs
@@ -0,0 +1,13 @@
+﻿class Licorne
+{
+   // Propriétés
+   private int taille = 170;
+   public string puissance = "Double.infinity";
+
+   // Méthodes
+   private void Dormir()
+   {}
+
+   public void Courir()
+   {}
+}
---
+++ P2/P2C0/Program.cs
@@ -0,0 +1,5 @@
+﻿Licorne licorne = new Licorne();
+Console.WriteLine(licorne.puissance); // OK
+licorne.taille = 180; // Erreur
+licorne.Dormir(); // Erreur
+licorne.Courir(); // OK
