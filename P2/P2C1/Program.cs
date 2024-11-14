ec592e9d1372d56783d56d23b878da3ca38a598f
Lingatsu <115959500+Lingatsu@users.noreply.github.com>
2024-11-14 17:03:23 +0100

    Exercice P2C1 terminé

--- P2/P2C1/Program.cs
+++ P2/P2C1/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿public class HelloWorld
+{
+    static void AfficheBonjour()
+    {
+        Console.WriteLine("Bonjour le monde !");
+    }
+    static void Main(string[] args)
+    {
+        AfficheBonjour();
+    }
+}
