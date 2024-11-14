c3cd18b18bfa5657ca5120403be6cb0d5f86b94d
Lingatsu <115959500+Lingatsu@users.noreply.github.com>
2024-11-14 18:13:39 +0100

    Exercice P2C3 terminé

--- P2/P2C3/Program.cs
+++ P2/P2C3/Program.cs
@@ -1,1 +1,32 @@
 ﻿//TODO : Créer une boucle for et while qui affiche à 5 reprises la phrase, « Je m'amuse comme un fou ! ».
+// Partie 1
+/*
+for(int i=0; i<5; i++)
+{
+    System.Console.WriteLine("Je m'amuse comme un fou !");
+}
+*/
+
+// Partie 2
+// Changez votre boucle « for » par une boucle « while ». Était-ce une bonne idée ?
+/*
+int i = 0;
+while(i<5)
+{
+    i += 1;
+    System.Console.WriteLine("Je m'amuse comme un fou !");
+}
+*/
+
+// Partie 3
+// Sautez l'affichage de la quatrième boucle.
+int i = 0;
+while(i<5)
+{
+    i += 1;
+    if(i==4)
+    {
+        continue;
+    }
+    System.Console.WriteLine("Je m'amuse comme un fou !");
+}
